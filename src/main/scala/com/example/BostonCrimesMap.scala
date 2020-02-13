841fee9f3f695eebce3e6c9f70327189183aa532
igr1978 <i.gavako@gmail.com>
2020-02-13 10:56:00 +0200

    BostonCrimesMap.scala

--- src/main/scala/com/example/BostonCrimesMap.scala
+++ src/main/scala/com/example/BostonCrimesMap.scala
@@ -11,6 +11,9 @@ import org.apache.spark.sql.functions.{callUDF, lit, map, _}
 object BostonCrimesMap extends App{
 
   Logger.getLogger("org").setLevel(Level.OFF)
+
+  val t1 = System.nanoTime
+
   if (args.length != 3) {
     println("Incorrect arguments.")
     println("Usage: /path/to/jar {path/to/crime.csv} {path/to/offense_codes.csv} {path/to/output_folder}")
@@ -44,7 +47,11 @@ object BostonCrimesMap extends App{
     .option("header", "true")
     .option("inferSchema", "true")
     .csv(offense_codes_file)
-    .withColumn("crime_type", trim(split($"NAME", "-")(0)))
+
+    .withColumn("NAME_SPLIT", trim(split($"NAME", "-")(0)))
+    .groupBy($"CODE")
+    .agg(first($"NAME_SPLIT").alias("crime_type"))
+    .orderBy($"CODE", $"crime_type")
 
   val offense_codes_broadcast: Broadcast[DataFrame] = spark.sparkContext.broadcast(offense_codes)
 
@@ -54,8 +61,6 @@ object BostonCrimesMap extends App{
     .na.fill(0.0)
     .cache
 
-  val window: WindowSpec = Window.partitionBy($"DISTRICT").orderBy($"count_crimes".desc)
-
   val crime_out_1: DataFrame = crime_new
     .groupBy($"DISTRICT", $"YEAR", $"MONTH")
     .agg(count($"INCIDENT_NUMBER").alias("count_crimes"))
@@ -64,18 +69,35 @@ object BostonCrimesMap extends App{
     .agg(sum($"count_crimes").alias("crimes_total"),
       callUDF("percentile_approx", $"count_crimes", lit(0.5)).alias("crimes_monthly"))
 
+  val window: WindowSpec = Window.partitionBy($"DISTRICT").orderBy($"count_crimes".desc)
   val crime_out_2: DataFrame = crime_new
     .groupBy($"DISTRICT", $"crime_type")
     .agg(count($"INCIDENT_NUMBER").alias("count_crimes"))
+
     .withColumn("rn", row_number().over(window))
     .filter($"rn" <= 3)
     .drop($"rn")
 
     .groupBy(($"DISTRICT").alias("district"))
     .agg(collect_list($"crime_type").alias("crime_type_list"))
-
     .withColumn("frequent_crime_types", array_join($"crime_type_list", ", "))
     .drop($"crime_type_list")
+
+//  case class crime_temp(DISTRICT: String, crime_type: String, count_crimes: Long)
+//  val crime_out_2: DataFrame = crime_new
+//    .groupBy($"DISTRICT", $"crime_type")
+//    .agg(count($"INCIDENT_NUMBER").alias("count_crimes"))
+//    .as [crime_temp]
+//
+//    .groupByKey(x => x.DISTRICT)
+//    .flatMapGroups{
+//      case ( district, iter) => iter.toList.sortBy(x => -x.count_crimes).take(3)
+//    }
+//
+//    .groupBy(($"DISTRICT").alias("district"))
+//    .agg(collect_list($"crime_type").alias("crime_type_list"))
+//    .withColumn("frequent_crime_types", array_join($"crime_type_list", ", "))
+//    .drop($"crime_type_list")
 
   val crime_out_3: DataFrame = crime_new
     .groupBy(($"DISTRICT").alias("district"))
@@ -89,7 +111,7 @@ object BostonCrimesMap extends App{
     //.orderBy($"crimes_total".desc)
     .orderBy($"district")
 
-  crime_out.show()
+  crime_out.show(false)
 
   crime_out.repartition(1)
     .write
@@ -98,6 +120,9 @@ object BostonCrimesMap extends App{
     .parquet(out_folder)
 
   println("A parquet file created.")
+
+  val duration = (System.nanoTime - t1) / 1e9d
+  println("Duration: " + duration)
 
   spark.stop()
 
